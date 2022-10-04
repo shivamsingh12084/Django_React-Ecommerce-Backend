585807ee7a76f2cd2b2596f15b1393a82ae15535
shivamsingh12084 <shivamsingh12084@gmail.com>
2022-10-04 20:45:09 +0530

    Djagno and Django_rest and implementation of getProducts function

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+
+    #Third Party
+    'rest_framework',
+
+    #Local
+    'base.apps.BaseConfig',
+]   
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
