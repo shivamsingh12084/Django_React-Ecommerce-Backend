804585c8bc3fb255cf3e101adb8055840f9c0b5c
shivamsingh12084 <shivamsingh12084@gmail.com>
2022-10-05 11:00:18 +0530

    Serializing our ProductModel and modifing views accourding to serializers

--- backend/base/views.py
+++ backend/base/views.py
@@ -5,6 +5,8 @@ from .products import products
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from . models import Product
+from . serializers import ProductSerializer
 # Create your views here.
 @api_view()
 def getRoutes(request):
@@ -28,14 +30,13 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
 def getProduct(request, pk):
-    product = []
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
-    return Response(product)
+    product = Product.objects.get(_id = pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
